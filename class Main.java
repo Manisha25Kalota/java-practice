08b49745a0e5ff00659c96fc80ed08915d0dd69c
Manisha25Kalota <109798411+Manisha25Kalota@users.noreply.github.com>
2022-10-07 14:46:56 +0530

    Add files via upload

---
+++ class Main.java
@@ -0,0 +1,10 @@
+package com.universe
+public class Main{
+public Static void main(String [] args)
+//output
+System.out.print("*");
+System.out.print("**");
+System.out.print("***");
+System.out.print("****");
+System.out.print("*****");
+}
